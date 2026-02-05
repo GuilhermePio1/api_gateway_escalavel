bcbeeea6e6d8ffa065f68279bfa34b4381a327b9
GuilhermePio1 <guilherme.camara0210@gmail.com>
2026-02-05 18:07:58 -0300

    Initialize scalable API Gateway project with Spring Boot, adding `.gitignore`, Maven configuration, base structure, and required dependencies.

---
+++ api-gateway/src/main/java/com/portfolio/api_gateway/ApiGatewayEscalavelApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayEscalavelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayEscalavelApplication.class, args);
+	}
+
+}
